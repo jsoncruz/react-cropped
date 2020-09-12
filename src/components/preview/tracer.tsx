83c8038731eef69ae71e629931afd0c9cdf58356
Judson <judson.play1@gmail.com>
2020-09-12 01:52:32 -0300

    Tracer useEffect moved to root of the preview component

--- src/components/preview/tracer.tsx
+++ src/components/preview/tracer.tsx
@@ -1,17 +1,27 @@
 import React, { useImperativeHandle, useRef } from 'react';
 
-interface TracerProps extends React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement> {
-  test?: number;
-}
+type TracerProps = React.DetailedHTMLProps<React.CanvasHTMLAttributes<HTMLCanvasElement>, HTMLCanvasElement>;
 
 export interface ImperativeTracerProps {
   canvas: HTMLCanvasElement | null;
+  context: CanvasRenderingContext2D | null;
 }
 
 const Tracer = React.forwardRef<ImperativeTracerProps, TracerProps>((props, ref) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  useImperativeHandle(ref, () => ({ canvas: canvasRef.current }), []);
-  return <canvas ref={canvasRef} />;
+
+  useImperativeHandle(ref, () => ({
+    canvas: canvasRef.current,
+    context: canvasRef.current?.getContext('2d') ?? null,
+  }), []);
+
+  return (
+    <canvas
+      {...props}
+      ref={canvasRef}
+      style={{ display: 'none' }}
+    />
+  );
 });
 
 export default Tracer;
