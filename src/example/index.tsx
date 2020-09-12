56101b9418484624c080dd6ff7eb16286065e97d
Judson <judson.play1@gmail.com>
2020-09-12 01:53:09 -0300

    Example

--- src/example/index.tsx
+++ src/example/index.tsx
@@ -1,18 +1,33 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { FileInput, Preview } from '../components';
 import { ImperativeFileInputProps } from '../components/fileinput';
+import { ImperativePreviewProps } from '../components/preview/index';
 
 export default function Example() {
   const fileInputRef = useRef<ImperativeFileInputProps>(null);
+  const previewRef = useRef<ImperativePreviewProps>(null);
+
+  const handleCrop = useCallback(() => {
+    if (previewRef.current) {
+      previewRef.current.crop();
+    }
+  }, []);
+
+  const handleClear = useCallback(() => {
+    if (fileInputRef.current) {
+      fileInputRef.current.clear();
+    }
+  }, []);
 
   return (
     <>
-      <Preview generic />
+      <Preview ref={previewRef} generic />
       <FileInput ref={fileInputRef} allowed={['jpg', 'jpeg', 'png', 'gif']}>
         <button type="button">Carregar</button>
       </FileInput>
-      <button type="button" onClick={() => fileInputRef.current?.clear()}>Limpar</button>
+      <button type="button" onClick={handleCrop}>Cortar</button>
+      <button type="button" onClick={handleClear}>Limpar</button>
     </>
   );
 }
